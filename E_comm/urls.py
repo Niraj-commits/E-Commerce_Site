c3d71ab836e4d5b8b659954156442211fc91d16a
niraj-commits <karki.ni018@gmail.com>
2025-02-27 18:20:06 +0545

    Added My Category and edit/delete category pages

--- E_comm/urls.py
+++ E_comm/urls.py
@@ -5,13 +5,16 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('favicon.ico/', lambda request: HttpResponse(status=204)),
-    path('', index,name='index'),
+    path('', home,name='index'),
     path('contact/',contact,name='contact'),
-    path('user_detail/',myProducts,name = 'myProducts'),
-    path('create/',NewItemCreate,name='create_item'),
-    path('category/',NewCategoryCreate,name='create_category'),
-    path('<pk>/',item_detail,name = 'item_details'),
-    path('<pk>/delete/',deleteItem,name = 'delete_items'),
-    path('<pk>/edit/',editProducts,name = 'edit_items'),
+    path('user_detail/',MyProducts,name = 'myProducts'),
+    path('create/',NewProduct,name='create_item'),
+    path('mycategories/',NewCategory,name='create_category'),
+    path('category/',ViewCategory,name='MyCategories'),
+    path('category/<pk>',EditCategory,name='edit_category'),
+    path('category/<pk>/delete',DeleteCategory,name='delete_category'),
+    path('<pk>/',ViewProduct,name = 'item_details'),
+    path('<pk>/delete/',DeleteProducts,name = 'delete_items'),
+    path('<pk>/edit/',EditProducts,name = 'edit_items'),
 
 ]+static(settings.MEDIA_URL,document_root =settings.MEDIA_ROOT)
--- E_comm/views.py
+++ E_comm/views.py
@@ -2,29 +2,30 @@ from django.shortcuts import render,redirect,HttpResponse
 # Create your views here.
 from .models import *
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-def index(request):
+def home(request):
     items = Item.objects.filter(is_sold = False)
     category = Category.objects.all()
     context = {
         "total_items":items,
         "categories":category
     }
-    return render(request,'index.html',context)
+    return render(request,'home.html',context)
 
 def contact(request):
     return render(request,'contact.html')
 
-def item_detail(request,pk):
+def ViewProduct(request,pk):
     
     item = Item.objects.get(pk = pk)
     related_items = Item.objects.filter(category = item.category,is_sold=False).exclude(pk = pk)[0:3]
     context = {'single_item':item,'related_items':related_items}
     
-    return render(request,'items/detail.html',context)
+    return render(request,'items/Products/detail.html',context)
 
 @login_required
-def NewItemCreate(request):
+def NewProduct(request):
     prev_category = Category.objects.all()
     context = {"category":prev_category}
     
@@ -39,10 +40,10 @@ def NewItemCreate(request):
         
         Item.objects.create(name = name,category = category,description= description,image= image,price = price,created_by = created_by)
         return redirect('/')
-    return render(request,'items/create.html',context)
+    return render(request,'items/Products/create.html',context)
 
 @login_required
-def deleteItem(request,pk):
+def DeleteProducts(request,pk):
     queryset = Item.objects.get(pk = pk)
     user = request.user
     if queryset.created_by == user:
@@ -50,14 +51,14 @@ def deleteItem(request,pk):
         return redirect('/')
 
 @login_required
-def myProducts(request):
+def MyProducts(request):
     user = request.user
     queryset = Item.objects.filter(created_by = user)
     context = {"items":queryset}
-    return render(request,'items/user_products.html',context)
+    return render(request,'items/Products/myProducts.html',context)
 
 @login_required
-def editProducts(request,pk):
+def EditProducts(request,pk):
     queryset = Item.objects.get(pk = pk)
     context = {"data":queryset}
     
@@ -75,10 +76,10 @@ def editProducts(request,pk):
         queryset.save()
         return redirect('item_details',pk=queryset.pk)
     
-    return render(request,'items/edit.html',context)
+    return render(request,'items/Products/edit.html',context)
 
 @login_required
-def NewCategoryCreate(request):
+def NewCategory(request): 
     
     if request.method == "POST":
         category = request.POST.get("category")
@@ -88,3 +89,39 @@ def NewCategoryCreate(request):
         return redirect('/')
         
     return render(request,'items/category/create.html')
+
+@login_required
+def ViewCategory(request):
+    user = request.user
+    queryset = Category.objects.filter(created_by = user)
+    context = {"categories":queryset}
+    return render(request,'items/category/myCategories.html',context)
+
+@login_required
+def EditCategory(request,pk):
+    queryset = Category.objects.get(pk = pk)
+    context = {"category":queryset}
+    
+    if request.method == "POST":
+        name = request.POST.get('category')
+        queryset.name = name
+        queryset.save()
+        return redirect("MyCategories")
+    
+    return render(request,"items/category/edit.html",context)
+
+@login_required
+def DeleteCategory(request,pk):
+    queryset = Category.objects.get(pk = pk)
+    
+    if Item.objects.filter(category = queryset).exists():
+        messages.error(request, "Sorry, this category is in use and cannot be deleted.")
+    
+    try:
+        queryset.delete()
+        messages.success(request, "Category deleted successfully.")
+    
+    except:
+        messages.error(request,"Error Occured")
+    
+    return redirect("MyCategories")
